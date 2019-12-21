333ee3da75b0ad9e9446469946d896f646ceac8e
zgg <940822429@qq.com>
2019-12-21 19:25:50 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
        // int[] arr = {0,0,1,1};
+        int[][] arr = {{0,0,0},{0,1,0},{0,0,0}};
         Solution s = new Solution();
-        System.out.println(s.uniquePaths3(2,3));
+        System.out.println(s.uniquePathsWithObstacles(arr));
 
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -173,6 +173,32 @@ public class Solution {
         return dp[n-1];
     }
 
+    //不同路径2：有障碍物
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        //初始化dp数组
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1){
+                break;
+            }
+            dp[i][0]=1;
+        }
+
+        for (int j = 0; j<n; j++){
+            if (obstacleGrid[0][j] == 1){
+                break;
+            }
+            dp[0][j]=1;
+        }
+        for (int i = 1;i < m; i++){
+            for (int j = 1; j < n;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 
 
 }
